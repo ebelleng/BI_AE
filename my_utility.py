9eceffc38b0563c0740a96256e726cdc88814608
ebelleng <etienne.bellenger@gmail.com>
2021-10-03 02:01:12 -0300

    train ae

--- my_utility.py
+++ my_utility.py
@@ -1,17 +1,34 @@
+'''
+INTEGRANTES
+ETIENNE BELLENGER HERRERA   17619315-8
+JUAN IGNACIO AVILA OJEDA    19013610-8
+'''
 # My Utility : auxiliars functions
 
 import pandas as pd
 import numpy  as np
 
 # Initialize weights
-def iniW(...):
-    #complete code
-    return(...)
+def iniW(hn,x):
+    size_input, _ = x.shape # 375, 5
+
+    r = np.sqrt(6 / ( hn + size_input))
+    w1 = np.random.randn((hn, size_input)) * 2* r - r    #dim -> (20x5)
+    w2 = np.random.randn(size_output, hn) * r           #dim -> (1x20)
+       
+    return w1,w2
 
 # STEP 1: Feed-forward of AE
 def forward_ae(x,w1,w2):	
-    #complete code
-	return(...)
+    # Calcula la activación de los Nodos Ocultos
+    z1 = np.dot(w1, x)
+    a1 = act_sigmoid(z1)
+    # Calcula la activación de los Nodos de Salida
+    z2 = np.dot(w2, a1)
+    a2 = act_sigmoid(z2)
+
+    return (a1,a2)
+
 #Activation function
 def act_sigmoid(z):
     return(1/(1+np.exp(-z)))   
@@ -20,9 +37,32 @@ def deriva_sigmoid(a):
     return(a*(1-a))
 
 # STEP 2: Feed-Backward
-def gradW_ae(a,x,w1,w2):    
-    #complete code
-    return(gW1,gW2)    
+def backward_ae(Act, x, w1, w2, mu):
+    # Valor calculado
+    _, a2 = Act 
+    # Calcular el error
+    error = a2 - x
+    # Calcular el gradiente oculto y salida    
+    dCdW = gradW_ae(Act, x, w2, error)
+    # Actualizar los pesos
+    w1, w2 = updW_ae(w1, w2, dCdW, mu)
+    # Calcular Error cuadratico medio
+    mse = np.sum(error[0] ** 2) / len(error[0])
+    return (w1,w2)
+
+def gradW_ae(Act,x,w2,e):    
+    a1, a2 = Act
+    z2 = deriva_sigmoid(a2) # 1, 375
+    z1 = deriva_sigmoid(a1) # 20, 375
+    
+    # Calcular gradiente decoder
+    delta2 = np.multiply(e, deriva_sigmoid(z2)) # Probar con a2
+    dCdW2 = np.dot(delta2, a1.T)
+    # Calcular gradiente 
+    delta1 = np.multiply( np.dot(w2.T, delta2), deriva_sigmoid(z1) )
+    dCdW1 = np.dot( delta1, x.T)
+
+    return dCdW1, dCdW2
 
 # Update W of the AE
 def updW_ae(w1,w2,gW1,gW2,mu):
@@ -77,8 +117,9 @@ def load_data_csv(fname):
 # save costo of Softmax and weights SAE 
 def save_w_dl(W,Ws,cost):    
     #complete code
+    return
    
 #load weight of the DL 
 def load_w_dl():
-    #complete code    
+    #complete code
     return(W)    
--- train.py
+++ train.py
@@ -7,23 +7,34 @@ import my_utility as ut
 	
 # Softmax's training
 def train_softmax(x,y,param):
-    w     = ut.iniW(...)
+    w     = ut.iniW(x)
     costo = []
     for iter in range(param[0]):        
-        # complete code        
+        # complete code
+        break  
     return(w,costo)
 
 # AE's Training 
 def train_ae(x,hnode,MaxIter,mu):
-    w1,w2 = ut.iniW(...)            
+    w1,w2 = ut.iniW(hnode,x)
+
     for iter in range(MaxIter):        
-        # complete code...        
-    return(w1)
+        #Step 1: Forward
+        Act = ut.forward_ae(x,w1,w2) 
+
+        #Step2: Backward
+        w1, w2 = ut.backward_ae(Act, x, w1, w2, mu) 
+
+    return (w1)
+    
 #SAE's Training 
-def train_sae(x,param):    
+def train_sae(x,param):
+    w1,w2 = ut.iniW(len(param), x)
     for hn in range(2,len(param)):
         print('AE={} Hnode={}'.format(hn-1,param[hn]))
-        # complete code... 
+        w1 = train_ae(x, len(param), param[0], param[1])
+        
+        W = []
     return(W,x) 
    
 # Beginning ...
